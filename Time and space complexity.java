82d8855fe1f593650dd2ff611000fd7423e6aefb
PayanshiJain <94756897+PayanshiJain@users.noreply.github.com>
2023-01-04 21:24:56 +0530

    Add files via upload

---
+++ Time and space complexity.java
@@ -0,0 +1,507 @@
+//BUBBLE SORT
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void bubbleSort(int[] arr) {
+    //write your code here
+    for(int itr=1;itr<=arr.length-1;itr++){
+        for(int j=0;j<arr.length-itr;j++){
+            if(isSmaller(arr,j+1,j)){
+                swap(arr,j+1,j);
+            }
+        }
+    }
+    
+  }
+
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  // return true if ith element is smaller than jth element
+  public static boolean isSmaller(int[] arr, int i, int j) {
+    System.out.println("Comparing " + arr[i] + " and " + arr[j]);
+    if (arr[i] < arr[j]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static void print(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    bubbleSort(arr);
+    print(arr);
+  }
+}
+
+
+
+
+//SELECTION SORT
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void selectionSort(int[] arr) {
+    //write your code here
+    for(int i=0;i<arr.length-1;i++){
+        int min=i; //min index m
+        
+        //for j
+        for(int j=i+1;j<arr.length;j++){
+            if(isSmaller(arr,j,min)){  //firstly comparison b/w j and min
+                min=j;
+            }
+        }
+        swap(arr,i,min);       //swap i and mi
+    }
+    
+  }
+
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  // return true if ith element is smaller than jth element
+  public static boolean isSmaller(int[] arr, int i, int j) {
+    System.out.println("Comparing " + arr[i] + " and " + arr[j]);
+    if (arr[i] < arr[j]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static void print(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    selectionSort(arr);
+    print(arr);
+  }
+
+}
+
+
+
+
+//INSERTION SORT
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void insertionSort(int[] arr) {
+    //write your code here
+    for(int i=1;i<arr.length;i++){
+        for(int j=i-1;j>=0;j--){
+            if(isGreater(arr,j,j+1)){
+                swap(arr,j,j+1);
+            }
+            else break;
+        }
+    }
+
+  }
+
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  // return true if jth element is greater than ith element
+  public static boolean isGreater(int[] arr, int j, int i) {
+    System.out.println("Comparing " + arr[i] + " and " + arr[j]);
+    if (arr[i] < arr[j]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static void print(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    insertionSort(arr);
+    print(arr);
+  }
+
+}
+
+
+
+
+
+
+//MERGE SORT
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static int[] mergeSort(int[] arr, int lo, int hi) {
+    //write your code here
+    
+    //base case
+    if(lo==hi){   //means only 1 element present
+        int ba[]= new int[1];
+        ba[0]= arr[lo];
+        return ba;
+    }
+    int mid=(lo+hi) /2;
+    
+    int fsh[]= mergeSort(arr,lo,mid);
+    int ssh[]= mergeSort(arr,mid+1,hi);
+    
+    int fsa[]= mergeTwoSortedArrays(fsh,ssh);
+    
+
+    return fsa;
+  }
+
+  //used for merging two sorted arrays
+  public static int[] mergeTwoSortedArrays(int[] a, int[] b){
+    System.out.println("Merging these two arrays ");
+    System.out.print("left array -> ");
+    print(a);
+    System.out.print("right array -> ");
+    print(b);
+    int i = 0, j =0, k = 0;
+    int[] ans = new int[a.length + b.length];
+    while(i < a.length && j < b.length){
+        if(a[i] <= b[j]){
+          ans[k] = a[i];
+          i++;
+          k++;
+        }else{
+          ans[k] = b[j];
+          j++;
+          k++;
+        }
+    }
+
+    while(i < a.length){
+      ans[k] = a[i];
+      k++;
+      i++;
+    }
+
+    while(j < b.length){
+      ans[k] = b[j];
+      k++;
+      j++;
+    }
+    
+    return ans;
+  }
+
+  public static void print(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    int[] sa = mergeSort(arr,0,arr.length - 1);
+    System.out.print("Sorted Array -> ");
+    print(sa);
+  }
+
+}
+
+
+
+
+
+//PARTITION AN ARRAY
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void partition(int[] arr, int pivot){
+    //write your code here
+    int i=0;
+    int j=0;
+    
+    //partitioning
+    //0 to j-1: <=pivot
+    //j to i-1: >pivot
+    //i to end: unknown
+    
+    while(i<arr.length){
+        if(arr[i]>pivot){
+            i++;
+        }
+        else{
+            swap(arr,i,j);
+            i++;
+            j++;
+        }
+    }
+    
+  }
+
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  public static void print(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    int pivot = scn.nextInt();
+    partition(arr,pivot);
+    print(arr);
+  }
+
+}
+
+
+
+
+
+
+//QUICK SORT
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void quickSort(int[] arr, int lo, int hi) {
+    //write your code here
+    if(lo>hi){
+        return;
+    }
+    int pivot=arr[hi];
+    int pi=partition(arr,pivot,lo,hi); //pivot index
+    quickSort(arr,lo,pi-1);
+    quickSort(arr,pi+1,hi);
+    
+  }
+
+  public static int partition(int[] arr, int pivot, int lo, int hi) {
+    System.out.println("pivot -> " + pivot);
+    int i = lo, j = lo;
+    while (i <= hi) {
+      if (arr[i] <= pivot) {
+        swap(arr, i, j);
+        i++;
+        j++;
+      } else {
+        i++;
+      }
+    }
+    System.out.println("pivot index -> " + (j - 1));
+    return (j - 1);
+  }
+
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  public static void print(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    quickSort(arr, 0, arr.length - 1);
+    print(arr);
+  }
+
+}
+
+
+
+
+//SORT-01
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void sort01(int[] arr){
+    //write your code here
+    int i=0;
+    int j=0;
+    
+    //i to end: unknown
+    //0 to j-1: zeros
+    //j to i-1: ones
+    
+    while(i<arr.length){
+        if(arr[i]==1){
+            i++;
+        }
+        else{
+            swap(arr,i,j);
+            i++;
+            j++;
+        }
+    }
+    
+  }
+
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping index " + i + " and index " + j);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  public static void print(int[] arr){
+    for(int i = 0 ; i < arr.length; i++){
+      System.out.println(arr[i]);
+    }
+  }
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0 ;i < n; i++){
+      arr[i] = scn.nextInt();
+    }
+    sort01(arr);
+    print(arr);
+  }
+
+}
+
+
+
+
+//SORT 012
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void sort012(int[] arr){
+    //write your code here
+    int i=0;
+    int j=0;
+    int k=arr.length-1;
+    
+    while(i<=k){
+        if(arr[i]==0){
+            swap(arr,i,j);
+            i++;
+            j++;
+        }
+        else if(arr[i]==1){
+            i++;
+        }
+        else{  //when arr[i]==2
+            swap(arr,i,k);
+            k--;
+ 
+        }
+    }
+    
+  }
+
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping index " + i + " and index " + j);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  public static void print(int[] arr){
+    for(int i = 0 ; i < arr.length; i++){
+      System.out.println(arr[i]);
+    }
+  }
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0 ;i < n; i++){
+      arr[i] = scn.nextInt();
+    }
+    sort012(arr);
+    print(arr);
+  }
+
+}
